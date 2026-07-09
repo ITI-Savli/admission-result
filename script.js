d2794c0e9119f53fc59dd115541f2364fceb44c5
ITI-Savli <prlsavaliiti@gmail.com>
2026-07-09 12:40:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+function searchResult(){
+
+let appNo=document.getElementById("appNo").value;
+
+fetch("https://script.google.com/macros/s/AKfycbzpMD7iFAFz2S1QcL8U0SJPu8i38bwaRZDnggqCpdumOSMLICkqrvsj0GKvyAa2dijA/exec?appNo="+encodeURIComponent(appNo))
+
+.then(response=>response.json())
+
+.then(data=>{
+
+if(data.found){
+
+document.getElementById("result").innerHTML=`
+
+<h2>Admission Result</h2>
+
+<p><b>Name:</b> ${data.name}</p>
+
+<p><b>Father:</b> ${data.father}</p>
+
+<p><b>Application:</b> ${data.application}</p>
+
+<p><b>Trade:</b> ${data.trade}</p>
+
+<p><b>ITI:</b> ${data.iti}</p>
+
+<p><b>Category:</b> ${data.category}</p>
+
+<p><b>Rank:</b> ${data.rank}</p>
+
+<p><b>Status:</b> ${data.status}</p>
+
+`;
+
+}
+
+else{
+
+document.getElementById("result").innerHTML="<h3>No Record Found</h3>";
+
+}
+
+});
+
+}
